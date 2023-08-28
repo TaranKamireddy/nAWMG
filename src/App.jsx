46bbac094fd25f37e30d2ef1368a50a75a85d61a
Meluhn <tarankamireddy@gmail.com>
2023-08-27 22:20:23 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/modern-normalize.css"
+import "./styles/App.css"
+import "./styles/hero.css"
+import "./styles/utils.css"
+
+export default function App() {
+  return (
+    <main className="hero container">
+      <h1>Hello World</h1>
+      <button className="btn">Button</button>
+    </main>
+  )
+}
